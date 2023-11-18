51b25236681ce37b2338f6b4d8c1a887ace47785
theusaf <theusafyt@gmail.com>
2023-11-17 23:16:10 -0800

    chore: build file

--- bc-seed-track-util.user.js
+++ bc-seed-track-util.user.js
@@ -11,27 +11,79 @@
 // @noframes
 // ==/UserScript==
 {
-    class TrackTree {
+    class TrackGraph {
         nodes = new Map();
         addNode(node, name) {
             this.nodes.set(name, node);
+        }
+        getNode(name) {
+            return this.nodes.get(name);
+        }
+        deleteNode(name) {
+            this.nodes.delete(name);
         }
     }
     class TrackGraphNode {
         neighbors = new Map();
         extraPullNode = null;
-        connectionType;
         element;
-        constructor(connectionType, element) {
-            this.connectionType = connectionType;
+        constructor(element) {
             this.element = element;
+        }
+        get catName() {
+            return this.element.querySelector("a").textContent;
+        }
+        get leadsToName() {
+            const outputNode = this.element.childNodes[3];
+            if (outputNode?.textContent.trim()) {
+                return outputNode.textContent.trim().match(/(\d+[ABR]+)/)[1];
+            }
+            else {
+                return null;
+            }
         }
     }
     const SINGLE_PULL_COST = 150;
     const ELEVEN_PULL_COST = 1500;
     const FIFTEEN_PULL_COST = 2100;
-    const FOUND_CAT_VALUE = 1500;
-    function djikstraSearch(graph, start) { }
+    function getCost(type) {
+        switch (type) {
+            case "normal":
+                return SINGLE_PULL_COST;
+            case "guaranteed11":
+                return ELEVEN_PULL_COST;
+            case "guaranteed15":
+                return FIFTEEN_PULL_COST;
+        }
+    }
+    function getSmallestVertex(distances, queue) {
+        let smallest = null;
+        let smallestDistance = Infinity;
+        for (const node of queue) {
+            const distance = distances.get(node);
+            if (distance < smallestDistance) {
+                smallest = node;
+                smallestDistance = distance;
+            }
+        }
+        return smallest;
+    }
+    function djikstraSearch(graph, start) {
+        const distances = new Map();
+        const previous = new Map();
+        const queue = new Set();
+        for (const node of graph.nodes.values()) {
+            distances.set(node, Infinity);
+            previous.set(node, null);
+            queue.add(node);
+        }
+        distances.set(start, 0);
+        while (queue.size > 0) {
+            const vertex = getSmallestVertex(distances, queue);
+            queue.delete(vertex);
+        }
+    }
+    // initial processing (convert DOM into arrays)
     function parseTable() {
         const header = document.querySelector("table tr:first-child"), itemsWanted = ["no.", "result", "score, slot", "guaranteed"], [mainNumber, mainResult, mainGuaranteed, altResult, altGuaranteed, altNumber,] = [...header.children]
             .map((child, i) => [i, child.textContent])
@@ -42,12 +94,14 @@
         const leftTrack = [], rightTrack = [];
         let leftTrackStartIndex = 0, leftTrackEndLength = 0, rightTrackStartIndex = 0, rightTrackEndLength = 0, rowsizes = [].fill(0, 0, altNumber + 1), columnSkip = 0;
         for (let r = 0; r < rows.length; r++) {
-            if (leftTrackStartIndex - r + leftTrackEndLength < 0 || leftTrackNumber === 0) {
+            if (leftTrackStartIndex - r + leftTrackEndLength < 0 ||
+                leftTrackNumber === 0) {
                 leftTrackStartIndex = r;
                 leftTrackEndLength = 0;
                 leftTrackNumber++;
             }
-            if (rightTrackStartIndex - r + rightTrackEndLength < 0 || rightTrackNumber === 0) {
+            if (rightTrackStartIndex - r + rightTrackEndLength < 0 ||
+                rightTrackNumber === 0) {
                 rightTrackStartIndex = r;
                 rightTrackEndLength = 0;
                 rightTrackNumber++;
@@ -81,7 +135,8 @@
                         break;
                     }
                     case mainResult: {
-                        if (cell.classList.contains("cat") && cell.classList.contains("pick")) {
+                        if (cell.classList.contains("cat") &&
+                            cell.classList.contains("pick")) {
                             if (leftTrack[leftTrackNumber - 1]) {
                                 leftTrack[leftTrackNumber - 1][0].push(cell);
                             }
@@ -92,13 +147,15 @@
                         break;
                     }
                     case mainGuaranteed: {
-                        if (cell.classList.contains("cat") && cell.classList.contains("pick")) {
+                        if (cell.classList.contains("cat") &&
+                            cell.classList.contains("pick")) {
                             leftTrack[leftTrackNumber - 1][1].push(cell);
                         }
                         break;
                     }
                     case altResult: {
-                        if (cell.classList.contains("cat") && cell.classList.contains("pick")) {
+                        if (cell.classList.contains("cat") &&
+                            cell.classList.contains("pick")) {
                             if (rightTrack[rightTrackNumber - 1]) {
                                 rightTrack[rightTrackNumber - 1][0].push(cell);
                             }
@@ -109,7 +166,8 @@
                         break;
                     }
                     case altGuaranteed: {
-                        if (cell.classList.contains("cat") && cell.classList.contains("pick")) {
+                        if (cell.classList.contains("cat") &&
+                            cell.classList.contains("pick")) {
                             rightTrack[rightTrackNumber - 1][1].push(cell);
                         }
                         break;
@@ -126,5 +184,65 @@
         }
         return { leftTrack, rightTrack };
     }
-    console.log(parseTable());
+    function generateGraph(leftTrack, rightTrack) {
+        const graph = new TrackGraph();
+        // nodify all items
+        for (let i = 0; i < leftTrack.length; i++) {
+            const [normal, guaranteed] = leftTrack[i];
+            for (let j = 0; j < normal.length; j++) {
+                const node = new TrackGraphNode(normal[j]);
+                graph.addNode(node, `${i + 1}A${j === 0 ? "" : "R"}`);
+            }
+            for (let j = 0; j < guaranteed.length; j++) {
+                const node = new TrackGraphNode(guaranteed[j]);
+                graph.addNode(node, `${i + 1}A${j === 0 ? "" : "R"}G`);
+            }
+        }
+        for (let i = 0; i < rightTrack.length; i++) {
+            const [normal, guaranteed] = rightTrack[i];
+            for (let j = 0; j < normal.length; j++) {
+                const node = new TrackGraphNode(normal[j]);
+                graph.addNode(node, `${i + 1}B${j === 0 ? "" : "R"}`);
+            }
+            for (let j = 0; j < guaranteed.length; j++) {
+                const node = new TrackGraphNode(guaranteed[j]);
+                graph.addNode(node, `${i + 1}B${j === 0 ? "" : "R"}G`);
+            }
+        }
+        // connect all items
+        for (let i = 0; i < leftTrack.length; i++) {
+            const [normal] = leftTrack[i];
+            for (let j = 0; j < normal.length; j++) {
+                const node = graph.getNode(`${i + 1}A${j === 0 ? "" : "R"}`);
+                const nodeCatName = node.catName;
+                // normal pull
+                const normalPullNextName = node.leadsToName ?? `${i + 2}A`;
+                const normalPullNext = graph.getNode(normalPullNextName);
+                if (normalPullNext) {
+                    const normalPullNextCatName = normalPullNext.catName;
+                    if (normalPullNextCatName === nodeCatName) {
+                        const nextName = `${i + 2}AR`;
+                        const next = graph.getNode(nextName);
+                        if (next) {
+                            node.neighbors.set(next, "normal");
+                            node.extraPullNode = next;
+                        }
+                    }
+                }
+                // guaranteed pull
+                const guaranteedPullNextName = node.leadsToName ?? `${i + 1}A${j === 0 ? "" : "R"}G`;
+                const guaranteedPullNext = graph.getNode(guaranteedPullNextName);
+                if (guaranteedPullNext) {
+                    const guaranteedPullLinkName = guaranteedPullNext.leadsToName, guaranteedLink = graph.getNode(guaranteedPullLinkName);
+                    const nameNumber = +guaranteedPullNextName.match(/(\d+)/)[1];
+                    node.neighbors.set(guaranteedLink, nameNumber - (i + 1) === 11 ? "guaranteed11" : "guaranteed15");
+                    graph.deleteNode(guaranteedPullNextName);
+                }
+            }
+        }
+        return graph;
+    }
+    const { leftTrack, rightTrack } = parseTable();
+    const graph = generateGraph(leftTrack, rightTrack);
+    const results = djikstraSearch(graph, graph.getNode("1A"));
 }
